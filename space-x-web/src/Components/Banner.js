c8480a5163b595f855d34818a7c04fc98bc20dc6
Arg51~ <anuragy554@gmail.com>
2023-03-04 01:28:07 +0530

    basic context api to scroll to section implmented

--- space-x-web/src/Components/Banner.js
+++ space-x-web/src/Components/Banner.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import S from "../Assets/Space-X.jpg";
 import "../Styles/Banner.css";
+import { MainContext } from "../ContextApi/MainContext";
 const Banner = () => {
-
-//   console.log(scrolly, MainContext, "scrolly");
+    let { scrolly } = useContext(MainContext);
   return (
     <div className="relative bg-gray-900 h-screen">
       <img
@@ -19,16 +19,18 @@ const Banner = () => {
             <span className="text-indigo-600"> Space-X Capsules</span>
           </h1>
           <div className="mt-8 d-flex align-items-center justify-center">
-            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
               <svg
-                class="fill-current w-4 h-4 mr-2"
+                className="fill-current w-4 h-4 mr-2"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
                 <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
               </svg>
               <span
-             
+             onClick={() => {
+                scrolly();
+              }}
               >
                 Take Me there
               </span>
